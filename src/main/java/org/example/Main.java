19eadb85476e8d9f94311e808e33cfdf1d03746c
Sofie Van Dingenen <sofie.van.dingenen@iths.se>
2024-06-18 17:05:50 +0200

    made small changes to test the game.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,19 +7,19 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         JFrame myframe = new JFrame("minesweeper");
-        Game minesweeper = new Game(10,10);
+        Game minesweeper = new Game();
+
+        // should not be resizeable ( dimensions wil be off)
+
 
 
       //  System.out.println(minesweeper.getNUM_OF_MINES());
         //System.out.println(minesweeper.getMineCells().size());
-        for(Cell mine : minesweeper.getMineCells()){
-            System.out.println(mine.INDEX);
-        }
         myframe.setSize(minesweeper.ROW*40+30,minesweeper.COLUMN*40+50);
 
         myframe.add(new GamePanel(minesweeper));
 
         myframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        myframe.setVisible(true);
+        myframe.show();
     }
 }
