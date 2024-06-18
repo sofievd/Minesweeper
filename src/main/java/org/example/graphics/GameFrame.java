6938e1bc18d46043d3be87402461d5c753a41940
Sofie Van Dingenen <sofie.van.dingenen@iths.se>
2024-06-18 17:03:13 +0200

    Created GameFrame class, is for adding methods that creates the frame for the game.

---
+++ src/main/java/org/example/graphics/GameFrame.java
@@ -0,0 +1,15 @@
+package org.example.graphics;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameFrame extends JFrame {
+    //TODO: add methods to create the frame of the game:
+    //it's size, place on screen and possibility to change the size of the window.
+    public GameFrame(String title) throws HeadlessException {
+        super(title);
+
+    }
+
+
+}
